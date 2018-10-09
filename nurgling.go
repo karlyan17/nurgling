ac62af8af93fd022fd6b86aca8e706e74395d121
karlyan17 <karlyan.kamerer@gmail.com>
2018-10-09 22:17:03 +0200

    stable connection

--- nurgling.go
+++ nurgling.go
@@ -10,14 +10,35 @@ func main() {
 	if err != nil {
 		fmt.Printf("error connecting to socket:\n")
 		fmt.Print(err)
+	} else {
+		fmt.Printf("Opened socket on port 7777 to listen on\n")
 	}
 	for {
 		connect, err := listen.Accept()
 		if err != nil {
 			fmt.Printf("connection error:\n")
 			fmt.Print(err)
+		} else {
+			fmt.Printf("start listening\n")
 		}
-		connect.Write([]byte("HTTP/1.1 200 OK\n\nSUCC"))
+		
+		message := make([]byte, 1024)
+		nbytes, err :=connect.Read(message)
+		if err != nil {
+			fmt.Printf("reading error (%v bytes read):\n", nbytes)
+			fmt.Print(err)
+		} else {
+			fmt.Printf("read %v bytes:\n", nbytes)
+			fmt.Print(string(message))
+		}
+
+		nbytes, err = connect.Write([]byte("HTTP/1.1 200 OK\n\nSUCC"))
+		if err != nil {
+			fmt.Printf("response error (%v bytes were written):\n", nbytes)
+			fmt.Print(err)
+		} else {
+			fmt.Printf("%v bytes written SUCCessfully\n", nbytes)
+		}
 		connect.Close()
 	}
 }
